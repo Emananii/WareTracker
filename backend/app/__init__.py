9baaed1106a574ae3470137ae5f3e1714c776504
Emmanuel Wambugu <emmanuelwambugu5@gmail.com>
2025-06-30 02:20:53 +0300

    Fixed trailing slash in netlify cors link

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -49,7 +49,7 @@ def create_app():
     CORS(app, resources={r"/*": {"origins": [
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://waretracker.netlify.app/"
+        "https://waretracker.netlify.app"
     ]}})
 
     
